3a865aa4543fb3ae0f2d7b2a3da803cd0cbc30e8
Berviller <71489790+Berviller@users.noreply.github.com>
2021-10-01 11:45:01 +0200

    Add files via upload

--- ProfileService/src/main/java/com/example/ProfileService/ProfileController.java
+++ ProfileService/src/main/java/com/example/ProfileService/ProfileController.java
@@ -2,16 +2,14 @@ package com.example.ProfileService;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ProfileController {
     private final AtomicLong counter = new AtomicLong();
     private final Map<Long, Profile> profiles = new HashMap<>();
-    
+    private final Set<String> emails = new HashSet<>();
 
     @GetMapping("/PS/profiles")
     public Collection<Profile> profiles(){
@@ -23,10 +21,12 @@ public class ProfileController {
     // + email, description, ...
     @PutMapping("/PS/profiles")
     public Profile profiles_put(@RequestBody Profile profile){
-        //String email = profile.getEmail();
+        if (emails.contains(profile.getEmail()))
+            throw new EmailInUseException(profile.getEmail());
         long new_id = counter.incrementAndGet();
         profile.setId(new_id);
         profiles.put(new_id, profile);
+        emails.add(profile.getEmail());
         return profile;
     }
 
@@ -34,6 +34,7 @@ public class ProfileController {
     public void profile_delete(
             @PathVariable(value = "id") Long id) {
         Profile profile = profiles.get(id);
+        emails.remove(profile.getEmail());
         profiles.remove(id);
     }
 
