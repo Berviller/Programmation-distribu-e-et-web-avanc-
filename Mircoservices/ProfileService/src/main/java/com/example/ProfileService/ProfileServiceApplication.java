c1e3e10838a3b4dfe2359cd665f58298e62e31ba
Berviller <71489790+Berviller@users.noreply.github.com>
2021-10-01 09:15:07 +0200

    Add files via upload

--- Mircoservices/ProfileService/src/main/java/com/example/ProfileService/ProfileServiceApplication.java
+++ Mircoservices/ProfileService/src/main/java/com/example/ProfileService/ProfileServiceApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController x
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileServiceApplication.class, args);
 	}
 
+}
+
+@GetMapping("/hello")
+public String hello(
+		@Requestparam(value = "name", defaultValue = "World") String name)
+{
+	return String.format("Hello %s!", name);
 }
