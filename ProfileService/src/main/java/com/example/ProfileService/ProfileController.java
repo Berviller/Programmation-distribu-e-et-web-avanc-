374f0d9cc19c9bf46e328c41cb9b144339fce184
Berviller <71489790+Berviller@users.noreply.github.com>
2021-10-01 11:17:43 +0200

    Add files via upload

--- ProfileService/src/main/java/com/example/ProfileService/ProfileController.java
+++ ProfileService/src/main/java/com/example/ProfileService/ProfileController.java
@@ -1,8 +1,6 @@
 package com.example.ProfileService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -20,11 +18,29 @@ public class ProfileController {
     //GET /PS/profiles/{id}
     //GET /PS/profiles/{id}/name
     //PUT /PS/profiles/{id}/name
+    // + email, description, ...
     @PutMapping("/PS/profiles")
     public Profile profiles_put(@RequestBody Profile profile){
+        String email = profile.getEmail();
         long new_id = counter.incrementAndGet();
         profile.setId(new_id);
         profiles.put(new_id, profile);
         return profile;
+    }
+
+    @DeleteMapping("/PS/profiles/{id}")
+    public void profile_delete(
+            @PathVariable(value = "id") Long id) {
+        Profile profile = profiles.get(id);
+        profiles.remove(id);
+    }
+
+    @GetMapping("/PS/profiles/{id}/name")
+    public void update_name(
+            @PathVariable(value = "id") Long id,
+            @RequestBody String name)
+    {
+        Profile profile = profiles.get(id);
+        profile.setName(name);
     }
 }
