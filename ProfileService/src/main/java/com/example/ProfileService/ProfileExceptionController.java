fa03d5a52357ef6fd80a0d92d315a0d32d945fde
Berviller <71489790+Berviller@users.noreply.github.com>
2021-10-05 10:57:29 +0200

    Add files via upload

--- ProfileService/src/main/java/com/example/ProfileService/ProfileExceptionController.java
+++ ProfileService/src/main/java/com/example/ProfileService/ProfileExceptionController.java
@@ -40,4 +40,6 @@ public class ProfileExceptionController {
         }
         return errors;
     }
+
+
 }
--- ProfileService/src/test/java/com/example/ProfileService/ProfileServiceApplicationTests.java
+++ ProfileService/src/test/java/com/example/ProfileService/ProfileServiceApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.ProfileService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static  org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static  org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static  org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class ProfileServiceApplicationTests {
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class ProfileServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void getProfilesShouldReturnEmptyArray() throws Exception{
+		this.mockMvc.perform(get("/PS/profiles"))
+            .andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json("[]"));
 	}
 
+	@Test
+	public void putProfileShouldSucceed() throws Exception {
+	Profile profile = new
+			Profile(1, "Nom", "test@example.com");
+	ObjectMapper objectMapper = new ObjectMapper();
+	String profile_json = objectMapper.writeValueAsString(profile);
+
+    this.mockMvc.perform(put("/PS/profiles")
+			.content(profile_json)
+    .contentType(MediaType.APPLICATION_JSON))
+
+				.andDo(print())
+			.andExpect(status().isOk());
+	}
 }
